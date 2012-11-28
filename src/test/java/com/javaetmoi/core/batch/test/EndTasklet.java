2497e8087628d3db0afcb766d19ed8765ce496ef
Antoine Rey <antoine.rey@free.fr>
2012-11-28 18:53:44 +0100

    Display the masterpieces number that have been processed by unpartitioned steps

--- src/test/java/com/javaetmoi/core/batch/test/EndTasklet.java
+++ src/test/java/com/javaetmoi/core/batch/test/EndTasklet.java
@@ -13,21 +13,52 @@
  */
 package com.javaetmoi.core.batch.test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.partition.support.DefaultStepExecutionAggregator;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+/**
+ * Display the masterpieces number that have been processed by unpartitioned steps.
+ * 
+ */
 public class EndTasklet implements Tasklet {
 
     private static final Logger LOG = LoggerFactory.getLogger(EndTasklet.class);
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) {
-        LOG.info("Last step of the batch");
+        JobExecution jobExecution = chunkContext.getStepContext().getStepExecution().getJobExecution();
+        StepExecution resume = getUnpartitionedStepResume(jobExecution);
+        LOG.info("{} masterpiece(s) have been processed", resume.getWriteCount());
         return RepeatStatus.FINISHED;
+    }
+
+    private StepExecution getUnpartitionedStepResume(JobExecution jobExecution) {
+        StepExecution result = new StepExecution("resume", jobExecution);
+        new DefaultStepExecutionAggregator().aggregate(result,
+                getUnpartitionedStepExecution(jobExecution));
+        return result;
+    }
+
+    private List<StepExecution> getUnpartitionedStepExecution(JobExecution jobExecution) {
+        List<StepExecution> unpartitionedStepExecution = new ArrayList<StepExecution>();
+        Collection<StepExecution> allStepExecutions = jobExecution.getStepExecutions();
+        for (StepExecution stepExecution : allStepExecutions) {
+            if (!stepExecution.getStepName().contains(":partition")) {
+                unpartitionedStepExecution.add(stepExecution);
+            }
+        }
+        return unpartitionedStepExecution;
     }
 
 }
