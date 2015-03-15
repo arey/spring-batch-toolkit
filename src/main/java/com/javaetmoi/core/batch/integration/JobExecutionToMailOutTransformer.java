23adbf807aea05387feeebdd59d8dfb13c5b5faf
Antoine Rey <antoine.rey@free.fr>
2015-03-15 18:02:35 +0100

    Add JobExecutionToMailOutTransformer

---
+++ src/main/java/com/javaetmoi/core/batch/integration/JobExecutionToMailOutTransformer.java
@@ -0,0 +1,47 @@
+package com.javaetmoi.core.batch.integration;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.Transformer;
+
+import java.io.File;
+import java.io.IOException;
+
+import static java.lang.System.lineSeparator;
+
+/**
+ * Transformer that accepts a Message whose payload is a JobExecution launched with an input.file parameter.
+ * <p>
+ * Mail body is build with an optional introduction string followed by JobExecution properties.
+ * </p>
+ */
+@MessageEndpoint
+public class JobExecutionToMailOutTransformer {
+
+    public String mailIntroduction;
+
+    @Transformer
+    public String toMailBody(JobExecution jobExecution) throws IOException {
+        String inputFile = jobExecution.getJobParameters().getString("input.file");
+        if (inputFile.contains("file://")) {
+            inputFile = inputFile.replace("file://", "");
+        }
+        String filename = new File(inputFile).getName();
+
+        StringBuilder body = new StringBuilder();
+        if (mailIntroduction != null) {
+            body.append(mailIntroduction);
+        }
+        body.append("Filename: ").append(filename).append("").append(lineSeparator());
+        body.append("Cause: ").append(jobExecution.getAllFailureExceptions()).append(lineSeparator());
+        body.append("Job Id: ").append(jobExecution.getJobId()).append(lineSeparator());
+        body.append("Start time: ").append(jobExecution.getStartTime()).append(lineSeparator());
+        body.append("End time: ").append(jobExecution.getEndTime()).append(lineSeparator());
+        body.append(lineSeparator());
+        return body.toString();
+    }
+
+    public void setMailIntroduction(String mailIntroduction) {
+        this.mailIntroduction = mailIntroduction;
+    }
+}
